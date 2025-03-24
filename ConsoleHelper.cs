aea88e1a42e3fbb5a7c9e89c5010b24e29d8e108
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-03-24 15:55:52 +0700

    Update ConsoleHelper.cs

--- ConsoleHelper.cs
+++ ConsoleHelper.cs
@@ -7,14 +7,5 @@
             Console.WriteLine("Нажмите ВВОД для продолжения ...");
             Console.ReadLine();
         }
-
-        public static bool IsExit() {
-            string command = ConsoleCommands.COMMANDS[-1];
-
-            Console.WriteLine($"Для выхода введите: {command}");
-            string input = Console.ReadLine();
-
-            return ConsoleCommands.CheckCommand(-1, input);
-        }
     }
 }
