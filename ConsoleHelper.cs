048b49aaa7f28203252209796ff38cddd2625aec
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-03-24 15:55:33 +0700

    Add files via upload

---
+++ ConsoleHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Utils
+{
+    public static class ConsoleHelper
+    {
+        public static void EnterForContinued()
+        {
+            Console.WriteLine("Нажмите ВВОД для продолжения ...");
+            Console.ReadLine();
+        }
+
+        public static bool IsExit() {
+            string command = ConsoleCommands.COMMANDS[-1];
+
+            Console.WriteLine($"Для выхода введите: {command}");
+            string input = Console.ReadLine();
+
+            return ConsoleCommands.CheckCommand(-1, input);
+        }
+    }
+}
